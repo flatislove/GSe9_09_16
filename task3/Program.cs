bf0c4bcbbfccd1842ed90376ec64c88ed229f0ea
flvtisv <flatislove@gmail.com>
2022-09-18 14:25:54 +0600

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,29 @@
+﻿//Задайте значения M и N. Напишите программу, которая найдёт наибольший общий делитель (НОД) этих чисел с помощью рекурсии.
+
+int ReadNumber(string message)
+{
+    int number = 0;
+    while (true)
+    {
+        Console.WriteLine("Введите число " + message);
+        if (!int.TryParse(Console.ReadLine(), out number))
+        {
+            Console.WriteLine("Некорректный ввод");
+        }
+        else break;
+    }
+    return number;
+}
+
+int GCDNumbers(int a, int b)
+{
+    if (b == 0)
+    {
+        return a;
+    }
+    else return GCDNumbers(b, a % b);
+}
+
+Console.Clear();
+Console.WriteLine("Нахождение НОД");
+Console.WriteLine(GCDNumbers(ReadNumber("M"), ReadNumber("N")));
